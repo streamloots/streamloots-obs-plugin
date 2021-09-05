73d9c5dd1e8586182fd13a4798d4c34a8805bfb7
eslem <j.eslem03@gmail.com>
2021-09-05 14:04:16 +0200

    basic source creation

---
+++ Makefile
@@ -0,0 +1,77 @@
+PluginName = "Streamloots"
+
+QtBaseDir := C:\Users\eslem\Code\Companies\StreamLoots\poc\obs-plugin\deps\QT
+QTDIR32 := $(QtBaseDir)\5.10.1\msvc2017
+QTDIR64 := $(QtBaseDir)\5.10.1\msvc2017_64
+CMAKE_PREFIX_PATH := $(QTDIR32)\lib\cmake\Qt5Widgets
+
+DepsBasePath := C:\Users\eslem\Code\Companies\StreamLoots\poc\obs-plugin\deps\dependencies2019
+DepsPath64 := $(DepsBasePath)\win64
+DepsPath32 := $(DepsBasePath)\win32
+
+OBSPath:= C:\Users\eslem\Code\Companies\StreamLoots\poc\obs-plugin\deps\obs-studio
+
+RESTSDKPath = C:\Users\eslem\Code\Companies\StreamLoots\poc\obs-plugin\deps\cpprestsdk\Release
+build_config := RelWithDebInfo
+
+msbuild:=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe
+
+install-qt:
+	SET "QtBaseDir=$(QtBaseDir)" && \
+	ci\windows\install-qt-win.cmd
+
+download-obs-deps:
+	SET "DepsBasePath=$(DepsBasePath)" && \
+	ci\windows\download-obs-deps.cmd
+
+prepare-obs-windows:
+	SET "build_config=$(build_config)" && \
+	SET "DepsPath32=$(DepsPath32)" && \
+	SET "DepsPath64=$(DepsPath64)" && \
+	SET "QTDIR32=$(QTDIR32)" && \
+	SET "QTDIR64=$(QTDIR64)" && \
+	SET "CMAKE_PREFIX_PATH=$(CMAKE_PREFIX_PATH)" && \
+	SET "OBSPath=$(OBSPath)" && \
+	ci\windows\prepare-obs-windows.cmd
+
+build-obs-32:
+	$(msbuild) /m /p:Configuration=$(build_config) $(OBSPath)\build32\obs-studio.sln
+
+build-obs-64:
+	$(msbuild) /m /p:Configuration=$(build_config) $(OBSPath)\build64\obs-studio.sln
+
+prepare-windows:
+	SET "build_config=$(build_config)" && \
+	SET "QTDIR32=$(QTDIR32)" && \
+	SET "QTDIR64=$(QTDIR64)" && \
+	SET "OBSPath=$(OBSPath)" && \
+	SET "cpprestsdk_DIR=$(RESTSDKPath)" && \
+	ci\windows\prepare-windows.cmd
+
+build-plugin-32:
+	$(msbuild) /m /p:Configuration=$(build_config) .\build32\main.sln
+
+build-plugin-64:
+	$(msbuild) /m /p:Configuration=$(build_config) .\build64\main.sln
+
+package-windows:
+	ci\windows\package-windows.cmd
+
+clean:
+	if exist build32 rd /s /q build32
+	if exist build64 rd /s /q build64
+	if exist release rd /s /q release
+
+build:
+	$(MAKE) clean
+	$(MAKE) prepare-windows
+	$(MAKE) build-plugin-32
+	$(MAKE) build-plugin-64
+	$(MAKE) package-windows
+
+build-install:
+	$(MAKE) clean
+	$(MAKE) prepare-windows
+	$(MAKE) build-plugin-32
+	$(MAKE) build-plugin-64
+	
--- src/plugin-main.c
+++ src/plugin-main.c
@@ -17,15 +17,125 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 */
 
 #include <obs-module.h>
-
+#include <obs-frontend-api.h>
+#include <obs-data.h>
 #include "plugin-macros.generated.h"
+#include <graphics/vec2.h>
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
 
+obs_source_t *create_img_source(){
+    obs_data_t *settings = obs_data_create_from_json("{\"url\":\"https:\/\/i.giphy.com\/media\/MFVI64gIopnOTDeeO9\/giphy.webp\"}");
+    
+    obs_source_t *source = obs_source_create("browser_source", "pepitogrillo", settings, NULL);
+    return source;
+}
+
+
+obs_scene_t *get_current_scene(){
+    obs_source_t *scene_source = obs_frontend_get_current_scene();
+    if(!scene_source){
+        blog(LOG_INFO, "No current scene");
+    }else{
+        blog(LOG_INFO, "current scene found");
+        char *name = obs_source_get_name(scene_source);
+        blog(LOG_INFO, "Current scene %s", name);
+        obs_scene_t  *scene = obs_scene_from_source(scene_source);
+        return scene;
+    }
+}
+
+obs_sceneitem_t *add_source_to_scene(obs_scene_t *scene, obs_source_t *source){
+    blog(LOG_INFO, "adding source to scene");
+    obs_sceneitem_t *scene_item = obs_scene_add(scene, source);
+    if(!scene_item){
+        blog(LOG_INFO, "Cannot add item");
+    }
+
+    struct vec2 position =  {0.0f, 1.0f};
+    struct vec2 scale =  {2.0f, 2.0f};
+    obs_sceneitem_set_pos(scene_item, &position);
+    obs_sceneitem_set_scale(scene_item, &scale);
+    return scene_item;
+}
+
+void create_source(){
+    obs_scene_t *scene = get_current_scene();
+    if(scene){
+        blog(LOG_INFO, "ok scene current");
+        obs_source_t *img_source = create_img_source();
+        char *name = obs_source_get_name(img_source);
+        blog(LOG_INFO, "New source name  %s", name);
+        add_source_to_scene(scene, img_source);
+    }
+}
+
+bool on_item_iterate(void* privateData, obs_source_t* source){
+    obs_scene_t *scene = get_current_scene();
+    char *name = obs_source_get_name(source);
+    enum obs_source_type sourceType = obs_source_get_type(source);
+    char *typeString ="";
+    switch (sourceType) {
+		case OBS_SOURCE_TYPE_INPUT:
+			typeString = "input";
+			break;
+
+		case OBS_SOURCE_TYPE_FILTER:
+			typeString = "filter";
+			break;
+
+		case OBS_SOURCE_TYPE_TRANSITION:
+			typeString = "transition";
+			break;
+
+		case OBS_SOURCE_TYPE_SCENE:
+			typeString = "scene";
+			break;
+
+		default:
+			typeString = "unknown";
+			break;
+		}
+
+    char *id = obs_source_get_id(source);
+    blog(LOG_INFO, "source name  %s type %s id %s", name, typeString, id);
+
+    if(strcmp(name, "Mic/Aux")==0){
+        blog(LOG_INFO, "mutting mic");
+        obs_source_set_muted(source, true);
+    }
+    if(strcmp(name, "Video Capture Device")==0){
+        blog(LOG_INFO, "closing camera");
+        obs_sceneitem_t *scene_item = obs_scene_sceneitem_from_source(scene, source);
+        obs_sceneitem_set_visible(scene_item, false);
+        obs_sceneitem_set_visible(scene_item, true);
+        obs_source_t *filter = obs_source_get_filter_by_name(source, "Colour Key");
+        obs_source_set_enabled(filter, true);
+    }
+    return true;
+}
+
+void iterate_items(){
+    blog(LOG_INFO, "loading sources");
+    obs_enum_sources(on_item_iterate, NULL);
+}
+
+void on_front_loaded(enum obs_frontend_event event, void *private_data){
+    if (event == OBS_FRONTEND_EVENT_FINISHED_LOADING) {
+        blog(LOG_INFO, "front finished");
+        iterate_items();
+        create_source();
+    }
+
+        //blog(LOG_INFO, "err");
+} 
+
 bool obs_module_load(void)
 {
     blog(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
+    obs_frontend_add_event_callback(on_front_loaded, NULL);
+
     return true;
 }
 
